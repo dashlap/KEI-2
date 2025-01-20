64f244dae622aab2106e33521bbd3f15d4b3cb75
dashlap <94745136+dashlap@users.noreply.github.com>
2025-01-20 12:46:12 +0400

    Add files via upload

---
+++ JS button chаnging/index.js
@@ -0,0 +1,9 @@
+// let btn1 = document.querySelector('.btn1')
+// btn1.backgroundColor='brown'
+// document.fontSize = 18 + 'px'
+
+function btn1(){
+    let btn = document.querySelector('.red')
+    btn.style.backgroundColor='blue'
+    document.fontSize = 18 + 'px'
+}
